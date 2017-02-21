995ffa2c4aca3b8dbdd536b71ce5dd41c0b1f8d1
謝佳瑋 <10446005@ntub.edu.tw>
2017-02-22 03:35:01 +0800

    presenter

---
+++ MVPSample/MVPSample/Presenter.swift
@@ -0,0 +1,28 @@
+//
+//  Presenter.swift
+//  MVPSample
+//
+//  Created by 謝佳瑋 on 2017/2/22.
+//  Copyright © 2017年 謝佳瑋. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Presenter:ApiHelperDelegate{
+    var view:ViewController?
+    
+    init(delegate:ViewController){
+        ///畫面跟Presenter連結
+        self.view = delegate
+        
+    }
+    
+    func apiSuccess(dataType:ApiActionType,api:ApiHelper){
+        
+    }
+    
+    func apiError(error: Error,errorType:ApiErrorType){
+        self.view?.showErrorMessage(errorType.rawValue)
+    }
+}
